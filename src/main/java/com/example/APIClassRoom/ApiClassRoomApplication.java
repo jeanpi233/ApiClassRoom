e10563cd92fca689aceeec4d35c975faca56959e
perrohjpt <tumamamelamama@gmail.com>
2025-06-04 10:10:11 -0500

    se hecn los get y post

--- src/main/java/com/example/APIClassRoom/ApiClassRoomApplication.java
+++ src/main/java/com/example/APIClassRoom/ApiClassRoomApplication.java
@@ -2,6 +2,9 @@ package com.example.APIClassRoom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiClassRoomApplication {
@@ -10,4 +13,19 @@ public class ApiClassRoomApplication {
 		SpringApplication.run(ApiClassRoomApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")  // Permite todas las rutas
+						.allowedOrigins("*")  // Permite cualquier origen
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+						.allowedHeaders("*");  // Permite todos los encabezados
+			}
+		};
+	}
+
 }
+
